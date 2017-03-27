133758e599e25fc4bfaa60f0dd4df7d0d1854d5c
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-03-27 09:27:22 +0200

    Update init script for new release

--- init_fcc_stack.sh
+++ init_fcc_stack.sh
@@ -19,73 +19,97 @@ function add_to_path {
     eval export ${path_name}=${path_value}
 }
 
-platform='unknown'
 unamestr=`uname`
 
 if [[ "$unamestr" == 'Linux' ]]; then
     LCGPREFIX=/cvmfs/sft.cern.ch/lcg
     export FCCSWPATH=/cvmfs/fcc.cern.ch/sw/0.8
-    platform='Linux'
-    echo "Platform detected: $platform"
     if [[ -d "$LCGPREFIX" ]] ; then
-        platform_tag=$1
-        if [ -z "$platform_tag" ] ; then
-            platform_tag=x86_64-slc6-gcc49
-        fi
-        # Check if build type is set, if not default to release build
-        if [ -z "$BUILDTYPE" ] || [[ "$BUILDTYPE" == "Release" ]]; then
-            export BINARY_TAG=${platform_tag}-opt
-            export CMAKE_BUILD_TYPE="Release"
-        else
-            export BINARY_TAG=${platform_tag}-dbg
-            export CMAKE_BUILD_TYPE="Debug"
-        fi
-        # Set up Gaudi + Dependencies
-        export lcg_version=LCG_88
-        export LCGPATH=$LCGPREFIX/views/${lcg_version}/$BINARY_TAG
-        # Only source the lcg setup script if paths are not already set
-        # (necessary because if incompatible pythia install in view)
-        case ":$LD_LIBRARY_PATH:" in
-            *":$LCGPATH/lib64:"*) :;;       # Path is present do nothing
-            *) source $LCGPATH/setup.sh;;   # otherwise setup
-        esac
+        # First construct the full platform
+        platform=$1
+        os=`python $FCCSWPATH/tools/hsf_get_platform.py --get=os`
+        # once we support cc7 this should be used:
+        #if [[ $os =~ ^(cc7|slc6)$ ]]; then
+        if [[ "$os" = "slc6" ]]; then
+            arch=`python $FCCSWPATH/tools/hsf_get_platform.py --get=architecture`
+            default_compiler=gcc49
+            if [ -z "$platform" ] ; then
+                platform=${arch}-${os}-${default_compiler}
+            else
+                # Check the input
+                if [[ ! $platform =~ ^${arch}-${os}-gcc[0-9]+$ ]]; then
+                    echo "[WARNING] invalid input, defaulting to ${arch}-${os}-${default_compiler}"
+                    platform=${arch}-${os}-${default_compiler}
+                fi
+            fi
+            # Check if build type is set, if not default to release build
+            if [ -z "$BUILDTYPE" ] || [[ "$BUILDTYPE" == "Release" ]]; then
+                export BINARY_TAG=${platform}-opt
+                export CMAKE_BUILD_TYPE="ReleaseWithDebInfo"
+            else
+                export BINARY_TAG=${platform}-dbg
+                export CMAKE_BUILD_TYPE="Debug"
+            fi
+            # clean up tmp vars for platform detection
+            unset os; unset arch; unset default_compiler;unset platform
+            # Which LCG version do we want to use?
+            export lcg_version=LCG_88
+            export LCGPATH=$LCGPREFIX/views/${lcg_version}/$BINARY_TAG
+            if [[ ! -d "$LCGPATH" ]]; then
+                echo "[ERROR] selected platform (${platform}) not supported in LCG release ${lcg_version}."
+                return -1
+            fi
+            # Only source the lcg setup script if paths are not already set
+            # (necessary because if incompatible pythia install in view)
+            case ":$LD_LIBRARY_PATH:" in
+                *":$LCGPATH/lib64:"*) :;;       # Path is present do nothing
+                *) source $LCGPATH/setup.sh;;   # otherwise setup
+            esac
 
-        echo "Software taken from $FCCSWPATH and $LCGPATH"
-        # If podio or EDM not set locally already, take them from afs
-        if [ -z "$PODIO" ]; then
-            export PODIO=$FCCSWPATH/podio/0.6/$BINARY_TAG
-        else
-            echo "Take podio: $PODIO"
-        fi
-        if [ -z "$FCCEDM" ]; then
-            export FCCEDM=$FCCSWPATH/fcc-edm/0.5/$BINARY_TAG
-        else
-            echo "Take fcc-edm: $FCCEDM"
-        fi
-        if [ -z "$FCCPHYSICS" ]; then
-            export FCCPHYSICS=$FCCSWPATH/fcc-physics/0.2/$BINARY_TAG
-        fi
-        export FCCDAG=$FCCSWPATH/dag/0.1/$BINARY_TAG
-        export DELPHES_DIR=$FCCSWPATH/delphes/3.4.1pre02/$BINARY_TAG
-        export PYTHIA8_DIR=$LCGPATH
-        export PYTHIA8_XML=$LCGPATH/share/Pythia8/xmldoc
-        export PYTHIA8DATA=$PYTHIA8_XML
-        export HEPMC_PREFIX=$LCGPATH
+            echo "Software taken from $FCCSWPATH and $LCGPATH"
+            # If podio or EDM not set locally already, take them from afs
+            if [ -z "$PODIO" ]; then
+                export PODIO=$FCCSWPATH/podio/0.6/$BINARY_TAG
+            else
+                echo "Take podio: $PODIO"
+            fi
+            # only check once and assume that the fcc installs are consistently setup
+            if [[ ! -d "$PODIO" ]]; then
+                echo "[ERROR] selected platform (${platform}) not supported in FCC release ${FCCSWPATH}."
+                return -1
+            fi
 
-        # add DD4hep (workaround for missing DD4hepConfig in standard locations)
-        # this also currently sets up the ROOT environment (sources thisroot.sh)
-        export inithere=$PWD
-        cd $LCGPREFIX/releases/${lcg_version}/DD4hep/00-20/$BINARY_TAG
-        source bin/thisdd4hep.sh
-        cd $inithere
+            if [ -z "$FCCEDM" ]; then
+                export FCCEDM=$FCCSWPATH/fcc-edm/0.5/$BINARY_TAG
+            else
+                echo "Take fcc-edm: $FCCEDM"
+            fi
+            if [ -z "$FCCPHYSICS" ]; then
+                export FCCPHYSICS=$FCCSWPATH/fcc-physics/0.2/$BINARY_TAG
+            fi
+            export FCCDAG=$FCCSWPATH/dag/0.1/$BINARY_TAG
+            export DELPHES_DIR=$FCCSWPATH/delphes/3.4.1pre02/$BINARY_TAG
+            export PYTHIA8_DIR=$LCGPATH
+            export PYTHIA8_XML=$LCGPATH/share/Pythia8/xmldoc
+            export PYTHIA8DATA=$PYTHIA8_XML
+            export HEPMC_PREFIX=$LCGPATH
 
-        # add gaudi to cmake path:
-        # in case we want to use lhcb installation when ready:
-        # LHCBPATH=/cvmfs/lhcb.cern.ch/lib/lhcb
-        # add_to_path CMAKE_PREFIX_PATH $LHCBPATH/GAUDI/GAUDI_v28r1/$BINARY_TAG
-        add_to_path CMAKE_PREFIX_PATH $FCCSWPATH/gaudi/$BINARY_TAG
-        # add Geant4 data files
-        source /cvmfs/geant4.cern.ch/geant4/10.2/setup_g4datasets.sh
+            # add DD4hep (workaround for missing DD4hepConfig in standard locations)
+            # this also currently sets up the ROOT environment (sources thisroot.sh)
+            export inithere=$PWD
+            cd $LCGPREFIX/releases/${lcg_version}/DD4hep/00-20/$BINARY_TAG
+            source bin/thisdd4hep.sh
+            cd $inithere
+
+            # add gaudi to cmake path:
+            # in case we want to use lhcb installation when ready:
+            # if we go that route, we may need to set LCG_SYSTEM and LCG_releases_base in FCCSW CMake
+            # LHCBPATH=/cvmfs/lhcb.cern.ch/lib/lhcb
+            # add_to_path CMAKE_PREFIX_PATH $LHCBPATH/GAUDI/GAUDI_v28r1/$BINARY_TAG
+            add_to_path CMAKE_PREFIX_PATH $FCCSWPATH/gaudi/v28r2/$BINARY_TAG
+            # add Geant4 data files
+            source /cvmfs/geant4.cern.ch/geant4/10.2/setup_g4datasets.sh
+        fi
     else
         # cannot find afs / cvmfs: so get rid of this to avoid confusion
         unset FCCSWPATH
@@ -95,8 +119,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
     add_to_path LD_LIBRARY_PATH $PYTHIA8_DIR/lib
     add_to_path LD_LIBRARY_PATH $FCCPHYSICS/lib
 elif [[ "$unamestr" == 'Darwin' ]]; then
-    platform='Darwin'
-    echo "Platform detected: $platform"
+    echo "Platform detected: Darwin"
     add_to_path DYLD_LIBRARY_PATH $FCCEDM/lib
     add_to_path DYLD_LIBRARY_PATH $PODIO/lib
     add_to_path DYLD_LIBRARY_PATH $PYTHIA8_DIR/lib
@@ -115,6 +138,7 @@ add_to_path PATH $FCCPHYSICS/bin
 add_to_path CMAKE_PREFIX_PATH $FCCEDM
 add_to_path CMAKE_PREFIX_PATH $PODIO
 add_to_path CMAKE_PREFIX_PATH $PYTHIA8_DIR
+add_to_path CMAKE_PREFIX_PATH $FCCDAG
 if [ "$DELPHES_DIR" ]; then
     add_to_path CMAKE_PREFIX_PATH $DELPHES_DIR
 fi
